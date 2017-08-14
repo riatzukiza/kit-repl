a46c7a3ffeaca3612fc04c53e0cc002d41396756
Your Name <you@example.com>
2017-08-13 22:50:59 -0500

    added additional new line to . This should be the last commit for a while, and the terminal will work how I want it for the moment.

--- safe-compile.sh
+++ safe-compile.sh
@@ -1,3 +1,5 @@
+# It is kind of a waste of time to compile all the files every time when I only change one.
+# this is lazyness, I can do better.
 git add .;
 git commit -m "$1";
 sibilant ./src/*.sibilant -o ./js/;
