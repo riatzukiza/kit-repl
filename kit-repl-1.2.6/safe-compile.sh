d82a98c9c88974ac23a0555db7d9c6dd9249724d
Your Name <you@example.com>
2017-10-22 07:44:35 -0500

    minorupdatekit-repl to 1.0.7:redefined actor send as abstract method

--- kit-repl-1.2.6/safe-compile.sh
+++
@@ -1,7 +0,0 @@
-# It is kind of a waste of time to compile all the files every time when I only change one.
-# this is lazyness, I can do better.
-git add .;
-git commit -m "$1";
-sibilant ./src/*.sibilant -o ./js/;
-git add .;
-git commit -m "recompiled js";
